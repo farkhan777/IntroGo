011244c2ae9d0c2905a150c04b8f472634155022
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-01-25 06:21:46 +0700

    Pointers

---
+++ 07_Pointers/02_WhenToUsePointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 0
+	fmt.Println("x before", &x)
+	fmt.Println("x before", x)
+	foo(&x)
+	fmt.Println("x after", &x)
+	fmt.Println("x after", x)
+}
+
+func foo(y *int) {
+	fmt.Println("y before", y)
+	fmt.Println("y before", *y)
+	*y = 45
+	fmt.Println("y after", y)
+	fmt.Println("y after", *y)
+}
