c0fcb4d5889d9688a3218f8f22b296277c3e3d74
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-02-01 14:12:59 +0700

    Applications JSON Bcrypt

--- 08_Application/06_Bcrypt/main.go
+++ 08_Application/06_Bcrypt/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	_ "golang.org/x/crypto/bcrypt"
+)
 
 func main() {
-	fmt.Println("test")
+	password := "farkhan12345678"
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(password)
+	fmt.Println(bs)
+	fmt.Println(string(bs))
+
+	loginPassword := "farkhan12345678"
+	// 	err = bcrypt.CompareHashAndPassword(bs, []byte(loginPassword))
+	err = bcrypt.CompareHashAndPassword([]byte(bs), []byte(loginPassword))
+	if err != nil {
+		fmt.Println("You can't login!")
+		return
+	}
+
+	fmt.Println("You're logged in")
 }
