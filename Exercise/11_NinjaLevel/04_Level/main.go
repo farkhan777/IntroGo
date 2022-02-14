1d1121f88ba3b1821576ee3f3edd06bc5602c03d
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-02-14 17:52:46 +0700

    Error Handling Exercise

---
+++ Exercise/11_NinjaLevel/04_Level/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		// write your error code here
+		nme := fmt.Errorf("norgate math redux: square root of negative number: %v", f)
+		return 0, sqrtError{"50.2289 N", "99.465 W", nme}
+	}
+	return 42, nil
+}
