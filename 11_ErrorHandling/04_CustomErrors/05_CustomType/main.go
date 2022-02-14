09f5c28fb3ef8cefbeb10346a9cc4bb98094bcec
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-02-14 16:53:51 +0700

    Error Handling in Go

---
+++ 11_ErrorHandling/04_CustomErrors/05_CustomType/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type norgateMathError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (n norgateMathError) Error() string {
+	return fmt.Sprintf("a norgate math error occured: %v %v %v", n.lat, n.long, n.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		nme := fmt.Errorf("norgate math redux: square root of negative number: %v", f)
+		return 0, norgateMathError{"50.2289 N", "99.465 W", nme}
+	}
+	return 42, nil
+}
